f4e6825c3239d57a97afde74b236f89300b2c134
Gustavo Almeida <Gustavo Almeida>
2017-03-30 12:17:12 -0300

    [Fix] Correções de algumas coisas.

--- js/freelancer.js
+++ js/freelancer.js
@@ -111,20 +111,13 @@ function returnSkillSetObjects() {
     var skillObjectsArray = [];
     var defaultLength = 10;
     var backendObject = [{
-            'headline': '<a class="skill-clickable" id="netSkill" data-text="Desenvolvimento de diversos sistemas utilizand a plataforma .NET entre eles, um sistema interto de ERP desenvolvido em AngularJS nas camadas de Front-End,' +
-                'que era integrado em uma camada middleware contendo uma Web Api (.NET) responsável pelo gerenciamento de todos os módulos do ERP.</br> Como repositório do banco de dados e' +
-                'ORM utilizou-se o ADO.NET EntityFramework 5.</br> Na arquitetura, optou-se por desacoplar o front end do back-end, gerando camadas específicas para' +
-                'cada módulo do ERP e camcadas de back-end centralizadas na WebApi, de modo que o desenvolvimento e deploy de front e back-end fossem' +
-                ' independentes" href="#">.NET</a>',
+            'headline': '<a class="skill-clickable" id="netSkill" data-text="Desenvolvimento de diversos sistemas utilizando a plataforma .NET entre eles, um sistema interto de ERP desenvolvido em AngularJS nas camadas de Front-End, integrado a uma camada middleware contendo uma Web Api (.NET) responsável pelo gerenciamento de todos os módulos do ERP.</br></br> Como repositório do banco de dados e ORM utilizou-se o ADO.NET EntityFramework 6.</br> Na arquitetura, optou-se por desacoplar o front end do back-end, gerando camadas específicas para cada módulo do ERP e camcadas de back-end centralizadas na WebApi, de modo que o desenvolvimento e deploy de front e back-end fossem independentes" href="#">.NET</a>',
             'value': 8,
             'length': defaultLength,
             'description': 'Details 1'
         },
         {
-            'headline': '<a class="skill-clickable" id="pythonskill" data-text="Desenvolvimento de aplicação em Python 3.4 como linguagem dedesenvolvimento integrando o' +
-                'Flask Web Framework para o gerenciamento de sessões dos usuários e controle de permissão as views.</br> O Mysql serviu como banco relacional interno integrado com o ORM Peewee, por' +
-                'ser dinâmico e atuar como intermediário do banco no back-end da aplicação. Na autenticação utilizou-se o modulo JWT para criação do token de' +
-                'autenticação do usuário logado e interceptors do AngularJS para interceptar todas as requisições da aplicação para validação dos Tokens." href="#">' +
+            'headline': '<a class="skill-clickable" id="pythonskill" data-text="Desenvolvimento de aplicações Python 3.4 utilizando Flask Web Framework para o gerenciamento de sessões dos usuários e controle de permissão as views e integrando o Mysql como banco relacional interno orienteado ao ORM Peewee.</br></br> Implementação do modulo JWT para criação do token de autenticação do usuário logado e interceptors do AngularJS para interceptar todas as requisições da aplicação para validação dos Tokens." href="#">' +
                 'Python</<a>',
             'value': 5,
             'length': defaultLength,
@@ -133,29 +126,14 @@ function returnSkillSetObjects() {
     ];
 
     var frontEndObject = [{
-            'headline': '<a class="skill-clickable" data-text="Acredito que HTML5 e CSS3 são conhecimentos essenciais(e naturais) para qualquer' +
-                ' desenvolvedor web no mercado. </br>Através do desenvolvimento de diversas aplicações web pelos anos utilizando funcionalidades nativas' +
-                'dessas tecnologias e de  versões anteriores, tais como ' +
-                'canvas, elementos de reprodução multimídia, LocalStorage e SessionStorage considero ter um conhecimento avançado nessas linguagens de marcação' +
-                '"href="#">HTML5/CSS3</a>',
+            'headline': '<a class="skill-clickable" data-text="Conhecimento avançado na linguagem de marcação de texto HTML5 e linguagem de estilo CSS3 para criação de Websites responsíveis, cross-browsing e multi-plataformas.<div></br>•Experiência em desenvolvimento de designs UI / UX. </br>•Otimização do HTML da página para máxima performance e escalabilidade. </br>•Validação de formuláios por atributos HTML5. </br>•Conhecimento de Padrões Web e boas práticas. </br>•Principíos SEO e aderimento destes na aplicação.</div>" href="#">HTML5/CSS3</a>',
             'value': 8,
             'length': defaultLength,
             'description': 'Details 1'
         },
         {
-            'headline': '<a class="skill-clickable" data-text="Seguindo a mesma linha ds tecnologias HTML5/CSS3, javascript é outra linguagem de programação' +
-                'que TODO desenvolvedor web deve saber, por ser algo essencial para o dinâmismo das páginas web. </br>Posso afirmar ter um conhecimento avançado' +
-                ' em javascript e na biblioteca jquery pois em todos os projetos web que desenvolvi utilizei as normas de boas práticas na codificação deste,' +
-                'tais como prototipação de funcões, e padrões de projeto web" href="#">Javascript/JQuery</a>',
-            'value': 9,
-            'length': defaultLength,
-            'description': 'Details 2'
-        },
-        {
-            'headline': '<a class="skill-clickable" data-text="Utilizei AngularJS 2 integrado ao Ionic 2 em um projeto mobile que desenvolvi por conta para atender' +
-                ' a uma necessidade de uma empresa terceira. </br>Posso dizer que até agora gostei muito da nova arquitetura do Angular2 comaparado com seu' +
-                ' antecessor, e estou aprimorando meu conhecimento nesta nova tecnologia"  href="#">Angular2</a>',
-            'value': 5,
+            'headline': '<a class="skill-clickable" data-text="Conhecimento avançado em Javascript para desenvolvimento de modulos extensíveis, e utilização de bibliotecas para otimização de funcionalidades da aplicação.</br>•Proeficiência em desenvolvimento de requisições asíncronas, dinâmismo de módulos parciais da página e AJAX.</br>•Conhecimento em Modulo Loaders to Javascript, tal como Require.js.</br>•Conhecimento de ferramentas front-end de build tais como Grunt e Gulp.js.</br>•Proeficiência de desenvolvimento em frameworks e libraries robustas tais como AngularJS 2, Knockout JS, React Js e JQuery " href="#">Javascript</a>',
+            'value': 8,
             'length': defaultLength,
             'description': 'Details 2'
         }
